19325ed8247db4b46c1fd192617121f7c3e09609
kxx <132kxx@gmail.com>
2023-02-17 15:04:49 +0900

    Initial Commit

---
+++ MyGame/MyGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGameApp.swift
+//  MyGame
+//
+//  Created by kxx: on 2023/02/17.
+//
+
+import SwiftUI
+
+@main
+struct MyGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
